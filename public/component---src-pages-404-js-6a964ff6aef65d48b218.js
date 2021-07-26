c5ec530c77a7a8f60858e75662cdc4e86a3bf4e4
Elena Martínez Marín <elemartinezmarin@protonmail.com>
2021-07-26 22:58:47 +0200

    rm public

--- public/component---src-pages-404-js-6a964ff6aef65d48b218.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{w2l6:function(n,t,e){"use strict";e.r(t),e.d(t,"default",(function(){return r}));var u=e("q1tI"),o=e.n(u);function r(){return o.a.createElement("div",null,"404 - Not Found")}}}]);
-//# sourceMappingURL=component---src-pages-404-js-6a964ff6aef65d48b218.js.map
